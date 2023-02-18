f30f7e4506fcf24b12c91d96f2de3d20d3ea52e9
DankerooDuck <iamthedarin@gmail.com>
2023-02-18 17:54:01 -0500

    Add project files.

---
+++ Generator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordGenerator
+{
+    internal class Generator
+    {
+        public Generator(int length, bool uppercaseAZ, bool lowercaseAZ, bool numbers, bool specChars)
+        {
+            string password;
+
+            StringBuilder allowedChars = new StringBuilder();
+            if(uppercaseAZ == true)
+            {
+                allowedChars.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            }
+            if(lowercaseAZ == true)
+            {
+                allowedChars.Append("abcdefghijklmnopqrstuvwxyz");
+            }
+            if(numbers == true)
+            {
+                allowedChars.Append("0123456789");
+            }
+            if(specChars == true)
+            {
+                allowedChars.Append("!@#$%^&*");
+            }
+            string chars = allowedChars.ToString();
+            //Console.WriteLine(chars);
+            //Console.WriteLine(allowedChars.ToString());
+
+            //gen
+            password = GeneratePassword(length, chars);
+            Console.WriteLine("\n" + password);
+        }
+
+
+
+        public string GeneratePassword(int length, IEnumerable<char> chars)
+        {
+            if(length <= 0)
+            {
+                throw new ArgumentException("Length must be greater than 0.", "length");
+            }
+            if(length > int.MaxValue / 8)
+            {
+                throw new ArgumentException("Length exceeds maximum length.", "length");
+            }
+            if(chars == null)
+            {
+                throw new ArgumentNullException("Character Set Error.");
+            }
+
+            var charArray = chars.Distinct().ToArray();
+            if(charArray.Length == 0)
+            {
+                throw new ArgumentException("chars must not be empty", "chars");
+            }
+
+            var bytes = new byte[length * 8];
+            new RNGCryptoServiceProvider().GetBytes(bytes);
+            var result = new char[length];
+            for(int i = 0; i < length; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = charArray[value % (uint)charArray.Length];
+            }
+
+            return new string(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * Generate a password, yo
+ * DankerooDuck 2023
+ * Darin Roberts
+ */
+
+using System.Data;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            printInstructions();
+            continuePrompt();
+
+            bool cont = true;
+            while(cont == true)
+            {
+                askSpecifications();
+                cont = loopPrompt();
+            }
+            
+        }
+
+        static void printInstructions()
+        {
+            Console.WriteLine("\tThis program will generate a random complex password to your specification.");
+        }
+
+        static bool loopPrompt()
+        {
+            Console.WriteLine("\nPress any key to continue or input 0 to quit...\n");
+            string inValue = Console.ReadLine();
+            Console.Clear();
+            if(inValue.Equals("0"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+
+        static void continuePrompt()
+        {
+            
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void askSpecifications()
+        {
+            //variables
+            string inValue;
+            int length = -1; //default -1
+            bool uppercaseAZ = false; //default false
+            bool lowercaseAZ = false; //default false
+            bool numbers = false; //default false
+            bool specChars = false; //default false
+
+            //title
+            Console.WriteLine("\tGenerator\n");
+
+            //get length
+            Console.Write("Length: ");
+            inValue = Console.ReadLine();
+            length = int.Parse(inValue);
+
+            //A-Z
+            Console.Write("A-Z? (Y/N): ");
+            inValue = Console.ReadLine();
+            uppercaseAZ = parseYN(inValue);
+
+            //a-z
+            Console.Write("a-z? (Y/N): ");
+            inValue = Console.ReadLine();
+            lowercaseAZ = parseYN(inValue);
+
+            //0-9
+            Console.Write("0-9? (Y/N): ");
+            inValue = Console.ReadLine();
+            numbers = parseYN(inValue);
+
+            //!@#$%^&*
+            Console.Write("!@#$%^&* (Y/N): ");
+            inValue = Console.ReadLine();
+            specChars = parseYN(inValue);
+
+            //gen
+            generate(length, uppercaseAZ, lowercaseAZ, numbers, specChars);
+
+
+        }
+
+        static void generate(int length, bool uppercaseAZ, bool lowercaseAZ, bool numbers, bool specChars)
+        {
+            Generator pw = new Generator(length, uppercaseAZ, lowercaseAZ, numbers, specChars);
+        }
+
+        static bool parseYN(string inValue)
+        {
+            //input string inValue should be a Y or N
+            //if not Y or N, ask again
+            //if Y return true
+            //if N return false
+            if(inValue.ToUpper().Equals("Y"))
+            {
+                return true;
+            }
+            else if (inValue.ToUpper().Equals("N"))
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
